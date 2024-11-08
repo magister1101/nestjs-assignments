40a0b0f03f3ca7d5b2741fa3b8f3077d4d30c40e
MarkVanMortem <34596876+magister1101@users.noreply.github.com>
2024-11-09 06:01:29 +0800

    Completed Assignment 3: Factorial calculator

--- src/assignment/assignment.controller.ts
+++ src/assignment/assignment.controller.ts
@@ -3,23 +3,23 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('assignment')
 export class AssignmentController {
-  @Get('prime/:number')
-  checkPrime(@Param('number') number: string) {
-    const primeNumber = parseInt(number);
-    if (isNaN(primeNumber) || primeNumber < 1) {
+  @Get('factorial/:number')
+  calculateFactorial(@Param('number') number: string) {
+    const factorialNumber = parseInt(number);
+    if (isNaN(factorialNumber) || factorialNumber < 0) {
       return { error: 'Invalid input. Please input a positive integer' };
     }
 
-    const isPrime = this.isPrime(primeNumber);
-    return { isPrime };
+    const factorial = this.getFactorial(factorialNumber);
+    return { factorial };
   }
 
-  private isPrime(number: number): boolean {
-    if (number <= 1) return false;
-    if (number === 2) return true;
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-      if (number % i === 0) return false;
+  private getFactorial(number: number): number {
+    if (number === 0 || number === 1) return;
+    let result = 1;
+    for (let i = 2; i <= number; i++) {
+      result = result * i;
     }
-    return true;
+    return result;
   }
 }
