beab17643580b97a43f6a314aa012f5ad17e81ad
MarkVanMortem <34596876+magister1101@users.noreply.github.com>
2024-11-09 05:54:45 +0800

    Completed Assignment 2: Prime Numbers

--- src/assignment/assignment.controller.ts
+++ src/assignment/assignment.controller.ts
@@ -3,28 +3,6 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('assignment')
 export class AssignmentController {
-  @Get('fibonacci/:number')
-  getFibonacciSequence(@Param('number') number: string) {
-    const count = parseInt(number);
-    if (isNaN(count) || count < 1) {
-      return { error: 'Invalid input' };
-    }
-
-    const sequence = this.fibonacciSequence(count);
-    return { sequence };
-  }
-
-  private fibonacciSequence(count: number): number[] {
-    if (count == 1) return [0];
-    if (count == 2) return [0, 1];
-
-    const sequence = [0, 1];
-    for (let i = 2; i < count; i++) {
-      sequence[i] = sequence[i - 1] + sequence[i - 2];
-    }
-    return sequence;
-  }
-
   @Get('prime/:number')
   checkPrime(@Param('number') number: string) {
     const primeNumber = parseInt(number);
